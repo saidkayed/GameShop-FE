e279ffedb18763053b92768a870d5a10d190fe14
Your Name <you@example.com>
2021-03-19 23:02:46 +0100

    added more fetstures

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,9 @@ import { Routes, RouterModule } from '@angular/router';
 
 //Pages
 import {LandingPageComponent} from './landing-page/landing-page.component'
+import {LoginComponent} from './pages/login/login.component'
 
-const routes: Routes = [{path: '', component: LandingPageComponent}];
+const routes: Routes = [{path: '', component: LandingPageComponent}, {path: 'login', component: LoginComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
